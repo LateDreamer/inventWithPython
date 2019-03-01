b04540090270f45ab621b168cf5f6471ef09c58c
JIN YOUNG PARK <jinyoungpark@JINui-MacBookAir.local>
2019-03-01 18:10:06 +0900

    guess final

--- guess.py
+++ guess.py
@@ -3,9 +3,23 @@ import random
 
 chanceTaken=0
 userName=input('Hello! What is your name?')
-print('Well, '+userName+' I am thinking of a number between 1 and 20.')
+print('Well, '+userName+', I am thinking of a number between 1 and 20.')
 randomNumber = random.randint(1,20)
 
 for i in range(6):
-    guessedNumber=input('Take a guess')
-    
+    guessedNumber=int(input('Take a guess'))
+    chanceTaken += 1
+
+    if guessedNumber < randomNumber:
+        print('Your guess is too low.')
+    elif guessedNumber > randomNumber:
+        print('Your guess is too high.')
+    else:
+        break
+
+if guessedNumber==randomNumber:
+    chanceTaken=str(chanceTaken)
+    print('Good job, '+userName+'. You guessed my number in ' + chanceTaken+' guesses.')
+else:
+    randomNumber=str(randomNumber)
+    print('Nope. The number I was thinking of was '+randomNumber+'.')
